e44cc28c42e86cb2ac58ae421203fde018acd8d0
Atharva Kulkarni <64882461+atharva2109@users.noreply.github.com>
2021-09-23 13:51:49 +0530

    Add files via upload

---
+++ src/categories.js
@@ -0,0 +1,61 @@
+import  fruits from './images/fruitsveg.jpg'
+import  dairy from './images/dairymilk.jpg'
+import  seafood from './images/seafood.jpg'
+import  packaged from './images/packagedfoods.jpg'
+import  beverages from './images/beverages.jpg'
+import Homecare from './images/homecare.jpg'
+import  Skincare from './images/skincare.jpg'
+import  men from './images/men.jpg'
+import  women from './images/women.jpg'
+import  kids from './images/kids.jpg'
+import  electronics from './images/electronics.jpg'
+export const data={
+    cardData:[
+    {
+        name:'Fruits and Vegetables',
+        img:fruits
+
+    },
+    {
+        name:'Dairy and Bakery',
+        img:dairy
+    },
+    {
+        name:'Eggs Meat and Seafood',
+        img:seafood
+    },
+    {
+        name:'Packaged Foods',
+        img:packaged
+    },
+    {
+        name:'Beverages',
+        img:beverages
+    },
+    {
+        name:'Home Care',
+        img:Homecare
+    },
+    {
+        name:'Skin Care and Beauty',
+        img:Skincare
+    },
+    {
+        name:'Men',
+        img:men
+    },
+    {
+        name:'Women',
+        img:women
+    },
+    {
+        name:'Kids',
+        img:kids
+    },
+    {
+        name:'Electronics',
+        img:electronics
+    }
+
+]
+}
